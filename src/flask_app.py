d5e58c6bd7af69fd9ad9f83b3276e00e698579c3
laurkan <laurkanr@gmail.com>
2021-09-13 10:25:15 -0400

    tested that templating works

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World"
+
+@app.route("/search")
+def search_page():
+    return render_template("search.html", action="/results")
+
+@app.route("/results")
+def results_page():
+    return "Results will go here"
 
 if __name__ == '__main__':
     port = 5000
